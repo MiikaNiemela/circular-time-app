c6047536ac884e58cb01f84f2d12dff1b2495ae6
Miika Niemelä <miika@niemelat.com>
2024-12-04 22:58:19 +0200

    configure ESLint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'off',
+    'no-console': 'warn',
+    'prefer-const': 'error',
+  },
   extends: '@react-native',
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,10 @@ module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  semi: true,
   singleQuote: true,
+  tabWidth: 2,
   trailingComma: 'all',
 };
